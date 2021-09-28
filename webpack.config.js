888116a94ee3be95e0703c90a29611d1e078ab6d
chynnalew <chynnalew@yahoo.com>
2021-09-28 12:00:21 -0700

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist/'),
+    // publicPath: "/earth-vacation/"
   },
   devtool: 'eval-source-map',
   devServer: {               
